c4591fac2ab2c9da847fd68988bcc82ead829e88
RF <34131940+drrf@users.noreply.github.com>
2020-02-13 20:52:23 +0200

    Upload the files
    new upload

---
+++ website-blocker.py
@@ -0,0 +1,34 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp=r"hosts"
+hosts_path=r"/etc/hosts"	#for windows use the path: C:\Windows\System32\drivers\etc
+redirect="127.0.0.1"
+website_list=[	"www.facebook.com",
+				"facebook.com",
+				"www.youtube.com",
+				"youtube.com"	]
+
+am = dt(dt.now().year,dt.now().month,dt.now().day,9)
+pm = dt(dt.now().year,dt.now().month,dt.now().day,17)
+
+while True:
+    if (am < dt.now() < pm):
+        print("Time to work ...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Free time have a fun...")
+    time.sleep(5*60) #every 5 minute
